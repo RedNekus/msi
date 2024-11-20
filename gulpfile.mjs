1d8349c937f2d8a77b6652cc00bc25ea90274aea
RedNekus <fighterneko@gmail.com>
2024-11-20 19:01:44 +0300

    20.11

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,6 +4,8 @@ import gulpSass from 'gulp-sass';
 import pug  from 'gulp-pug';
 import browserify from 'browserify';
 import source from 'vinyl-source-stream';
+import eslint from 'gulp-eslint';
+import babelify from 'babelify';
 
 const sass = gulpSass(dartSass);
 
@@ -18,21 +20,38 @@ function buildHTML() {
         .pipe(pug({pretty: true}))
         .pipe(gulp.dest('./build/'));
 };
+
+function lintJs() {
+    return gulp.src('./src/js/script.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+};
+
 function buildJS() {
-    return browserify('./src/js/script.js')
+    return browserify({
+            debug: true,
+            entries: ['./src/js/script.js'],
+            transform: [
+                [
+                  "babelify"
+                ]
+            ]
+        })
         .bundle()
         .pipe(source('main.js'))
         .pipe(gulp.dest('./build/js/'));
 };
+
 function copyJS() {
     return gulp.src('./src/js/!(script).js')
-        .pipe(gulp.dest('./build/js/'))
+        .pipe(gulp.dest('./build/js/'));
 }
 
 const watch = function () {
     gulp.watch('./src/sass/**/*.scss',  gulp.series('buildStyles'));
     gulp.watch('./src/**/*.pug',  gulp.series('buildHTML'));
-    gulp.watch('./src/js/*.js',  gulp.series('buildJS', 'copyJS'))
+    gulp.watch('./src/js/*.js',  gulp.series('lintJs', 'buildJS', 'copyJS'))
 }
 
-export { buildStyles, buildHTML, buildJS, copyJS, watch };
+export { buildStyles, buildHTML, buildJS, lintJs, copyJS, watch };
--- src/js/script.js
+++ src/js/script.js
@@ -1,43 +1,39 @@
-//const Mask = require("imask");
+import iMask from 'imask';
+
 document.addEventListener('DOMContentLoaded', () => {
-    const dropdowns = document.querySelectorAll(`[data-toggle="dropdown"]`)
-    const editLink = document.querySelector(`[data-action="edit"]`)
-    const form = document.forms[0]
-    const elems = Object.values(form.elements).filter( el => el.classList.contains('is-editable') ) ?? []
-    editLink.addEventListener('click', (e) => {
-        e.preventDefault()
-        elems.forEach(elem => {
-            elem.classList.remove('lead-form__group--disabled')
-            let inputEl = elem.querySelector('input')
-            inputEl.removeAttribute('disabled')
-        })
+  const dropdowns = document.querySelectorAll(`[data-toggle="dropdown"]`);
+  const editLink = document.querySelector(`[data-action="edit"]`);
+  const form = document.forms[0];
+  const elems = Object.values(form.elements).
+      filter((el) => el && el.classList.contains('is-editable'));
+  editLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    elems.forEach((elem) => {
+      elem.classList.remove('lead-form__group--disabled');
+      const inputEl = elem.querySelector('input');
+      inputEl.removeAttribute('disabled');
     });
-    if(dropdowns) {
-        dropdowns.forEach(dropdown => {
-            dropdown.addEventListener('click', () => {
-                let contentId = dropdown.dataset?.target.replace('#', '')
-                if(null !== contentId) {
-                    let content = document.getElementById(contentId)
-                    content.classList.toggle('is-hidden')
-                    dropdown.classList.toggle('is-open')
-                }
-            });
-        });
-    };
-    /*let newMask = Mask.createMask(
-        document.getElementById('phone'),
-        {
-          mask: 'XXX-XX-0000',
-          definitions: {
-            X: {
-              mask: '0',
-              displayChar: 'X',
-              placeholderChar: '#',
-            },
-          },
-          lazy: false,
-          overwrite: 'shift',
+  });
+  if (dropdowns) {
+    dropdowns.forEach((dropdown) => {
+      dropdown.addEventListener('click', () => {
+        const contentId = dropdown.dataset.target.replace('#', '');
+        if (null !== contentId) {
+          const content = document.getElementById(contentId);
+          content.classList.toggle('is-hidden');
+          dropdown.classList.toggle('is-open');
         }
-    );
-    console.log(newMask);*/
-})
+      });
+    });
+  }
+  const newMask = iMask(
+      document.getElementById('phone'),
+      {
+        mask: Number,
+        min: 0,
+        max: 1000,
+      },
+  );
+  console.log(newMask);
+});
+
