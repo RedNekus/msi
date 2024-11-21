0596ce4db4ff5205ac86ca70f77477020527f8ec
RedNekus <fighterneko@gmail.com>
2024-11-21 18:29:49 +0300

    21.211

--- src/js/script.js
+++ src/js/script.js
@@ -4,17 +4,35 @@ document.addEventListener('DOMContentLoaded', () => {
   const dropdowns = document.querySelectorAll(`[data-toggle="dropdown"]`);
   const editLink = document.querySelector(`[data-action="edit"]`);
   const form = document.forms[0];
-  const elems = Object.values(form.elements).
-      filter((el) => el && el.classList.contains('is-editable'));
-  editLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    elems.forEach((elem) => {
-      elem.classList.remove('lead-form__group--disabled');
-      const inputEl = elem.querySelector('input');
-      inputEl.removeAttribute('disabled');
+  let elems = [];
+  if ('undefined' !== typeof form) {
+    elems = Object.values(form.elements).
+        filter((el) => el && el.classList.contains('is-editable'));
+    const phone = form.elements.phone;
+    if (null !== phone) {
+      const newMask = iMask(phone,
+          {
+            mask: '+{375} (00) 000-00-00',
+            lazy: true,
+          },
+      ).on('complete', function() {
+        // todo:
+      });
+      console.log(newMask);
+    }
+  }
+
+  if (null !== editLink) {
+    editLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      elems.forEach((elem) => {
+        elem.classList.remove('lead-form__group--disabled');
+        const inputEl = elem.querySelector('input');
+        inputEl.removeAttribute('disabled');
+      });
     });
-  });
-  if (dropdowns) {
+  }
+  if (null !== dropdowns) {
     dropdowns.forEach((dropdown) => {
       dropdown.addEventListener('click', () => {
         const contentId = dropdown.dataset.target.replace('#', '');
@@ -26,16 +44,4 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   }
-  const newMask = iMask(
-      document.getElementById('phone'),
-      {
-        mask: '+{375} (00) 000-00-00',
-        lazy: true,
-      },
-  ).on('complete', function() {
-    // phoneInput.classList.remove('is-invalid');
-    // phoneInput.classList.add('is-valid');
-    console.log('222');
-  });
-  console.log(newMask);
 });
