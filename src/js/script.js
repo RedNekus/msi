7e8f012151cfec5441582b900e78c6ee54bd3f4a
RedNekus <fighterneko@gmail.com>
2024-11-25 18:32:55 +0300

    25.11

--- src/js/script.js
+++ src/js/script.js
@@ -4,12 +4,33 @@ document.addEventListener('DOMContentLoaded', () => {
   const dropdowns = document.querySelectorAll(`[data-toggle="dropdown"]`);
   const editLink = document.querySelector(`[data-action="edit"]`);
   const form = document.forms[0];
+  const check = document.getElementById('decided');
   let elems = [];
+  check.addEventListener('change', (e) => {
+    if (check.checked) {
+      console.log('add');
+      form.elements.link.setAttribute('disabled', 'disabled');
+      form.elements.model.setAttribute('disabled', 'disabled');
+      form.elements.link.parentElement.
+          classList.add('lead-form__group--disabled');
+      form.elements.model.parentElement.
+          classList.add('lead-form__group--disabled');
+    } else {
+      console.log('remove');
+      form.elements.link.removeAttribute('disabled', 'disabled');
+      form.elements.model.removeAttribute('disabled', 'disabled');
+      form.elements.link.parentElement.
+          classList.remove('lead-form__group--disabled');
+      form.elements.model.parentElement.
+          classList.remove('lead-form__group--disabled');
+    }
+  });
   if ('undefined' !== typeof form) {
     elems = Object.values(form.elements).
         filter((el) => el && el.classList.contains('is-editable'));
     const phone = form.elements.phone;
-    if (null !== phone) {
+    console.log(phone);
+    if ('undefined' !== typeof phone) {
       const newMask = iMask(phone,
           {
             mask: '+{375} (00) 000-00-00',
@@ -28,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
       elems.forEach((elem) => {
         elem.classList.remove('lead-form__group--disabled');
         const inputEl = elem.querySelector('input');
-        inputEl.removeAttribute('disabled');
+        inputEl.removeAttribute('readonly');
       });
     });
   }
