2758c5665a6e7cce22181e421ac59b3f1e6e2f18
RedNekus <fighterneko@gmail.com>
2024-12-03 19:01:35 +0300

    03.12 - end

--- app/Http/Controllers/LeadsController.php
+++ app/Http/Controllers/LeadsController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Leads;
+use App\Models\Bitrix;
 
 class LeadsController extends Controller
 {
     public function get() {
-        return view('msi.leads', []);
+        $leads = Leads::all();
+        foreach($leads as $lead) {
+            //var_dump($lead);
+            //echo "test: {$lead->bx_lead_id}";
+            //Bitrix::getDealData($lead->id)
+        }
+        return view('msi.leads', ['leads' => $leads]);
     }
 }
