098205ab4fd216af50101144a7354b4d803b0260
RedNekus <fighterneko@gmail.com>
2025-06-23 18:00:28 +0300

    23.06

--- app/Http/Controllers/MsiController.php
+++ app/Http/Controllers/MsiController.php
@@ -106,7 +106,7 @@ class MsiController extends Controller
                 if(empty($yourlsData)) {
                     file_put_contents('sms-log.log', "Нет короткой ссылки {$stateArr[2]}\n", FILE_APPEND);
                 }
-                return ['data' => $yourlsData];
+                return ['data' => $yourlsData, 'sms' => $res];
             } else {
                 return ['data' => 'err 1'];
             }
