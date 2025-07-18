5b5962c966a22f54729659521d3973f716590ec5
RedNekus <fighterneko@gmail.com>
2025-07-18 12:47:21 +0300

    18.07

--- resources/views/msi.blade.php
+++ resources/views/msi.blade.php
@@ -1,6 +1,15 @@
 <?php
 $domain = $_SERVER['HTTP_HOST'];
 $protocol = 'https';
+
+// Генерация версий для файлов
+function asset_version($path) {
+    $fullPath = public_path($path);
+    if (file_exists($fullPath)) {
+        return $path . '?v=' . filemtime($fullPath); // можно заменить на md5_file($fullPath)
+    }
+    return $path;
+}
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -8,12 +17,16 @@ $protocol = 'https';
     <title>МСИ (Yoowhils)</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <script src="/js/main.js"></script>
+
+    <script src="{{ asset_version('js/main.js') }}"></script>
+
     <link href="https://fonts.cdnfonts.com/css/tt-norms-pro" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset_version('css/style.css') }}">
+
     <base href="{{$protocol}}://{{$domain}}">
     @hasSection('styles')
       @yield('styles')
